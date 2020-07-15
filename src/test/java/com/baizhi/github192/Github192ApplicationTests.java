7b57e5fae5cf03b30c2a77bdeed48a7f97bb2176
zhangcn <15236674712@163.com>
2020-07-15 16:50:03 +0800

    本地添加了一个主函数

--- src/test/java/com/baizhi/github192/Github192ApplicationTests.java
+++ src/test/java/com/baizhi/github192/Github192ApplicationTests.java
@@ -14,4 +14,11 @@ public class Github192ApplicationTests {
         System.out.println("==Github写的代码==");
     }
 
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        a=a+b;
+        System.out.println(a);
+    }
+
 }
