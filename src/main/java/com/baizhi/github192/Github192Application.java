040b722ea408ea6ee2ee1a5cbe1276af5950a65f
zhangcn <15236674712@163.com>
2020-07-15 16:40:45 +0800

    这是基础环境搭建版本 v1.0

---
+++ src/main/java/com/baizhi/github192/Github192Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.github192;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github192Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github192Application.class, args);
+    }
+
+}
